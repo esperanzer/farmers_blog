cc3f00e86e4fabdd7377c6ff470fde58a7529e3e
[esperanzer] <[esperanzatm95@gmail.com]>
2018-03-19 22:30:00 -0800

    modifying the homepage

--- farmers_blog/urls.py
+++ farmers_blog/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from home import urls as home_urls
 from django.conf.urls import url, include
-from home import urls as home_urls
 from django.contrib import admin
 
 urlpatterns = [
--- home/models.py
+++ home/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+	product_name = models.CharField(max_length= 55)
+	farm_name = models.CharField(max_length=55)
+	product_logo = models.CharField(max_length=400)
+	Country = models.CharField(max_length= 20)
+
+	#def__str__ (self)
+
